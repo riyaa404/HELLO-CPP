d93921724caf5605d6bf981e7d4e450d9db23fe6
riyaa404 <rr.riiya16@gmail.com>
2026-03-31 12:13:20 +0530

    function that prints primes in raange from 2 to n

---
+++ functions/P9.cpp
@@ -0,0 +1,40 @@
+// WAF to print all Primes in a range from 2 to n
+
+#include <iostream>
+using namespace std;
+
+void primes(int val)
+{
+    if(val <= 1)
+    {
+        cout << "No primes";
+        return;
+    }
+    for(int i=2; i<=val; i++)
+    {
+        bool isPrime = true;    // resets for each i
+
+        for(int j=2; j*j<=i; j++)
+        {
+            if(i % j == 0)
+            {
+                isPrime = false;
+                break;
+            }
+        }
+        if(isPrime)
+        {
+            cout << i << " ";
+        }
+    }
+}
+int main()
+{
+    int num;
+    cout << "Enter number : ";
+    cin >> num;
+
+    cout << "Primes in range : ";
+    primes(num);
+    return 0;
+}
