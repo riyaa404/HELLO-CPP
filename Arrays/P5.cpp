fe3923b02dafc077f8f2328b866aa5d6c92766fe
riyaa404 <rr.riiya16@gmail.com>
2026-04-09 14:12:51 +0530

    Reverse an array without using extra space

---
+++ Arrays/P5.cpp
@@ -0,0 +1,30 @@
+// Reverse an array without using extra space (2 Pointer Approach)
+// tc = O(n)    sc = O(1)
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int i, n=5;
+    int arr[5], rev[5];
+
+    cout << "Enter 5 Elements of Array : ";
+    for(i=0; i<n; i++)
+    {
+        cin >> arr[i];
+    }
+    int start = 0, end = 4;
+    while(start < end)
+    {
+        swap(arr[start], arr[end]);
+        start ++;
+        end --;
+    }
+    
+    cout << "Reverse Array : ";
+    for(int i=0; i<=4; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
