b1ca0cac5388a7e760449d1395ae5322fb624cdb
riyaa404 <rr.riiya16@gmail.com>
2026-04-09 19:20:09 +0530

    Binary Search - to search element in array

---
+++ Arrays/P6.cpp
@@ -0,0 +1,57 @@
+// Binary Search - has better time complexity than linear search
+// NOTE: the array should be sorted before applying binary search logic.
+
+#include <iostream> 
+using namespace std;
+
+int binarySearch(int *arr, int n, int key)
+{
+    int start = 0, end = n-1;
+
+    while(start <= end)
+    {
+        int mid = (start + end) / 2;
+
+        if(arr[mid] == key)
+        {
+            return mid;
+        }
+        else if(arr[mid] < key)
+        {
+            start = mid + 1;
+        }
+        else
+        {
+            end = mid - 1;
+        }
+    }
+    return -1;
+}
+
+int main()
+{
+    int i, key, n=5;
+    int arr[n];
+
+    cout << "Enter " << n << " Elements of Array (Sorted Array): ";
+    for(i=0; i<n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    cout << "Enter element to search : ";
+    cin >> key;
+
+    int idx = binarySearch(arr, n, key);
+
+    if(idx != -1)
+    {
+        cout << "\nElement is found at index " << idx;
+    }
+    else
+    {
+        cout << "\nElement is not found";
+    }
+
+    return 0;
+}
