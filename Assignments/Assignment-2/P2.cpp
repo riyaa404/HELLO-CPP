a1f59fe85716ea71b2f1ffabf19411a977534351
riyaa404 <rr.riiya16@gmail.com>
2026-04-09 10:52:47 +0530

    Print number in words (1-3) using switch-case with default handling

---
+++ Assignments/Assignment-2/P2.cpp
@@ -0,0 +1,26 @@
+// Q2.Repeat Q1 using switch case.Perform experiments.
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int num;
+    cout << "Enter Number : ";
+    cin >> num;
+
+    switch(num)
+    {
+        case 1 :
+            cout << "ONE";
+            break;
+        case 2 : 
+            cout << "TWO";
+            break;
+        case 3 :
+            cout << "THREE";
+            break;
+        default : 
+            cout << "Wrong Input";
+    }
+    return 0;
+}
