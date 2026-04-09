f4cddde19d05faa8cede51fce21c8b1fa4c977f6
riyaa404 <rr.riiya16@gmail.com>
2026-04-09 12:32:32 +0530

    Reverse an array -> with using extra space

---
+++ Arrays/P4.cpp
@@ -0,0 +1,34 @@
+// Reverse an array -> using extra space
+// tc = O(n)    sc = O(n)
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int i, n=5;
+    int arr[5], rev[5];
+
+    cout << "Enter 5 Elements of Array : ";
+    for(i=0; i<n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    for(i=0; i<n; i++)
+    {
+        rev[i] = arr[n-1-i];
+    }
+
+    cout << "Actual Array  : ";
+    for(i=0; i<n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+
+    cout << "\nReverse Array : ";
+    for(i=0; i<n; i++)
+    {
+        cout << rev[i] << " ";
+    }
+    return 0;
+}
